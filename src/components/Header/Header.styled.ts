2c03ebba8520948c6c110764b55a6e469732bf1e
fkozlicki <filip.kozlickii@gmail.com>
2022-07-03 14:57:40 +0200

    header components styles

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  height: 400px;
+  background: #dce2ef;
+  position: relative;
+  z-index: 0;
+
+  .wave {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+`;
