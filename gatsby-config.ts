7acab940625d6f453e2cce7ed2e92c792e50ef8b
fkozlicki <filip.kozlickii@gmail.com>
2022-07-04 08:51:24 +0200

    strapi connection and strapi config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,15 @@
 import type { GatsbyConfig } from 'gatsby';
+import dotenv from 'dotenv';
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ['post', 'category', 'author', 'comment'],
+  singleTypes: [],
+};
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -12,6 +23,10 @@ const config: GatsbyConfig = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
