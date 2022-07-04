7525b84c5bc9af2951c20ffcd3db49774fc99ae3
fkozlicki <filip.kozlickii@gmail.com>
2022-07-04 08:50:27 +0200

    Footer styles - styled components

---
+++ src/components/Footer/Footer.styled.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Background = styled.footer`
+  padding: 15px;
+  background: #dce2ef;
+`;
+export const Quote = styled.h2`
+  font-family: 'Permanent Marker', cursive;
+  font-size: 30px;
+  text-align: center;
+  margin-bottom: 15px;
+`;
+export const Author = styled.p`
+  text-align: center;
+  margin-bottom: 30px;
+`;
+export const Socials = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  font-size: 25px;
+  margin-bottom: 20px;
+`;
+export const Logo = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+export const Copy = styled.p`
+  text-align: center;
+  font-size: 14px;
+`;
