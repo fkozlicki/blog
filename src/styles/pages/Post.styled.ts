d0dd727ff1dd12e8562c74a6ed2eb0f61bd4dc0f
Filip Koźlicki <filip.kozlicki@dreamstormstudios.com>
2022-07-10 22:10:06 +0200

    Post page styles

---
+++ src/styles/pages/Post.styled.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Title = styled.article`
+  font-size: 40px;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 15px;
+`;
+export const Author = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  margin-bottom: 50px;
+
+  .author-image {
+    border-radius: 999px;
+    border: 3px solid blue;
+  }
+`;
+export const Categories = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 15px;
+`;
