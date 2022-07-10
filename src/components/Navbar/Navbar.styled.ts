84105d8af000fa5f70466a2e558ac0e24954ea6d
Filip Koźlicki <filip.kozlicki@dreamstormstudios.com>
2022-07-10 22:06:18 +0200

    changed styles

--- src/components/Navbar/Navbar.styled.ts
+++ src/components/Navbar/Navbar.styled.ts
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
 
+interface ISubMenuProps {
+  isOpen: boolean;
+}
+
+interface ISubMenuToggleBtnProps {
+  isSubMenuOpen: boolean;
+}
+
 export const Navbar = styled.nav`
   display: flex;
   padding: 10px 0;
+  margin-bottom: 30px;
 `;
 
 export const List = styled.ul`
@@ -23,7 +32,7 @@ export const List = styled.ul`
       height: 100%;
       display: flex;
       align-items: center;
-      font-weight: 400;
+      font-weight: 500;
       font-size: 16px;
     }
   }
@@ -47,5 +56,47 @@ export const ThemeButton = styled.button`
     display: inline-flex;
     align-items: center;
     font-size: 18px;
+  }
+`;
+
+export const Categories = styled.li`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  position: relative;
+`;
+
+export const SubMenuToggleBtn = styled.button<ISubMenuToggleBtnProps>`
+  font-size: 20px;
+  line-height: 0.5;
+  transform: ${(props) =>
+    props.isSubMenuOpen ? 'rotate(180deg)' : 'rotate(0)'};
+  transition: transform 250ms ease-out, background 100ms ease-out;
+  transform-origin: center;
+  border-radius: 9999px;
+
+  &:hover {
+    background: #00000020;
+  }
+`;
+
+export const SubMenu = styled.div<ISubMenuProps>`
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+  background: white;
+  padding: 10px;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+  opacity: ${(props) => (props.isOpen ? 1 : 0)};
+  visibility: ${(props) => (props.isOpen ? 'visible' : 'hidden')};
+  transition: opacity 150ms ease-out, visibility 150ms ease-out;
+
+  a {
+    padding: 5px;
   }
 `;
