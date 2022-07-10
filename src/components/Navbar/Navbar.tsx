04d9b33aa1879ae9de9c0ffffd91fa68b70b22b7
Filip Koźlicki <filip.kozlicki@dreamstormstudios.com>
2022-07-10 22:06:07 +0200

    dynamic categories and submenu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,36 @@
 import React, { useState } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import * as S from './Navbar.styled';
-import { BiMenu, BiMoon, BiSun, BiX } from 'react-icons/bi';
+import { BiChevronDown, BiMenu, BiMoon, BiSun, BiX } from 'react-icons/bi';
 import MobileMenu from './MobileMenu/MobileMenu';
 import { Container } from '../../styles/Global.styled';
+import slugify from 'slugify';
+
+const query = graphql`
+  {
+    allStrapiCategory {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 const Navbar = () => {
   const darkTheme = false;
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [subMenuOpen, setSubMenuOpen] = useState<boolean>(false);
+  const {
+    allStrapiCategory: { nodes: categories },
+  } = useStaticQuery(query);
 
   const toggleMenuOpen = (): void => {
     setMenuOpen(!menuOpen);
+  };
+
+  const toggleSubMenuOpen = (): void => {
+    setSubMenuOpen(!subMenuOpen);
   };
 
   return (
@@ -27,11 +46,27 @@ const Navbar = () => {
         </Link>
         <S.List>
           <li>
-            <Link to={`/latest`}>Ostatnie</Link>
+            <Link to={`/all`}>Wszystkie</Link>
           </li>
-          <li>
+          <S.Categories>
             <Link to={`/category`}>Kategorie</Link>
-          </li>
+            <S.SubMenuToggleBtn
+              isSubMenuOpen={subMenuOpen}
+              onClick={toggleSubMenuOpen}
+            >
+              <BiChevronDown />
+            </S.SubMenuToggleBtn>
+            <S.SubMenu isOpen={subMenuOpen}>
+              {categories.map((category: { name: string }) => (
+                <Link
+                  key={category.name}
+                  to={`/category/${slugify(category.name, { lower: true })}`}
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </S.SubMenu>
+          </S.Categories>
           <li>
             <Link to={`/contact`}>Kontakt</Link>
           </li>
@@ -42,7 +77,7 @@ const Navbar = () => {
         <S.ThemeButton>{darkTheme ? <BiMoon /> : <BiSun />}</S.ThemeButton>
 
         {/* MOBILE MENU */}
-        <MobileMenu open={menuOpen} />
+        <MobileMenu categories={categories} open={menuOpen} />
       </S.Navbar>
     </Container>
   );
