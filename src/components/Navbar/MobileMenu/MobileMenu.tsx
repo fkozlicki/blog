f541e3b4467312cbb8d751a85bb6fb46054ba7e4
Filip Koźlicki <filip.kozlicki@dreamstormstudios.com>
2022-07-10 22:04:49 +0200

    Dynamic categories

--- src/components/Navbar/MobileMenu/MobileMenu.tsx
+++ src/components/Navbar/MobileMenu/MobileMenu.tsx
@@ -1,21 +1,36 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import { BiMoon } from 'react-icons/bi';
+import slugify from 'slugify';
 import * as S from './MobileMenu.styled';
 
-const MobileMenu = ({ open }: { open: boolean }) => {
+interface IMobileMenuProps {
+  open: boolean;
+  categories: [];
+}
+
+const MobileMenu = ({ open, categories }: IMobileMenuProps) => {
   return (
     <S.MobileMenu open={open}>
       <S.Box>
         <ul>
           <li>
-            <Link to={`latest`}>Ostatnie</Link>
+            <Link to="/all">Wszystkie</Link>
           </li>
           <li>
-            <Link to={`categories`}>Kategorie</Link>
+            <Link to="/categories">Kategorie</Link>
           </li>
+          {categories.map((category: { name: string }) => (
+            <Link
+              key={category.name}
+              to={`/category/${slugify(category.name, { lower: true })}`}
+              className="category"
+            >
+              {category.name}
+            </Link>
+          ))}
           <li>
-            <Link to={`contact`}>Contact</Link>
+            <Link to="/contact">Kontakt</Link>
           </li>
         </ul>
         <S.ThemeButton>
