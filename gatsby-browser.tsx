7efad7510316126eccf44509134b93daa9e7d355
Filip Koźlicki <filip.kozlicki@dreamstormstudios.com>
2022-09-03 16:09:41 +0200

    Added wrappers

---
+++ gatsby-browser.tsx
@@ -0,0 +1,8 @@
+import RootElementWrapper from './src/components/RootElementWrapper';
+import PageElementWrapper from './src/components/PageElementWrapper';
+import 'prism-themes/themes/prism-coldark-dark.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+
+export const wrapRootElement = RootElementWrapper;
+
+export const wrapPageElement = PageElementWrapper;
