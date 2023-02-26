baaee360eee5c48b899cae4f307ec8d56dbf8d98
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-26 15:34:43 +0100

    Created filter section

--- src/pages/all.tsx
+++ src/pages/all.tsx
@@ -1,8 +1,9 @@
 import { graphql, PageProps } from 'gatsby';
-import React from 'react';
+import React, { useState } from 'react';
+import FilteredPosts from '../components/FilteredPosts/FilteredPosts';
 import Layout from '../components/Layout/Layout';
 import PostsSection from '../components/PostsSection/PostsSection';
-import { Container, Main } from '../styled/Utils';
+import Search from '../components/Search/Search';
 
 interface AllProps extends PageProps {
 	data: {
@@ -16,14 +17,61 @@ interface AllProps extends PageProps {
 				}
 			];
 		};
+		allStrapiCategory: { nodes: { name: string }[] };
 	};
 }
 
 const All: Component<AllProps> = ({ data }) => {
 	const posts = data.allStrapiPost.nodes;
+	const categories = data.allStrapiCategory.nodes.map(
+		(category) => category.name
+	);
+	const [input, setInput] = useState<string>('');
+	const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+	const filteredPosts = posts.filter((post) => {
+		const lowerCaseInput = input.toLowerCase();
+
+		if (selectedCategories.length > 0) {
+			return (
+				(post.title.toLowerCase().includes(lowerCaseInput) ||
+					post.excerpt.includes(lowerCaseInput)) &&
+				selectedCategories.some((selected) =>
+					post.categories.map((category) => category.name).includes(selected)
+				)
+			);
+		}
+		return (
+			post.title.toLowerCase().includes(lowerCaseInput) ||
+			post.excerpt.includes(lowerCaseInput)
+		);
+	});
+
+	const toggleSelectCategory = (selected: string) => {
+		if (selectedCategories.includes(selected)) {
+			setSelectedCategories((prev) =>
+				prev.filter((category) => category !== selected)
+			);
+		} else {
+			setSelectedCategories((prev) => [...prev, selected]);
+		}
+	};
+
+	const changeInput = (text: string) => {
+		setInput(text);
+	};
+
+	const isSelected = (selected: string) =>
+		selectedCategories.includes(selected);
+
 	return (
 		<Layout>
-			<PostsSection posts={posts} header="Wszystkie Posty" />
+			<Search
+				changeInput={changeInput}
+				categories={categories}
+				toggleSelectCategory={toggleSelectCategory}
+				isSelected={isSelected}
+			/>
+			<FilteredPosts posts={filteredPosts} />
 		</Layout>
 	);
 };
@@ -40,6 +88,11 @@ export const query = graphql`
 				categories {
 					name
 				}
+			}
+		}
+		allStrapiCategory {
+			nodes {
+				name
 			}
 		}
 	}
