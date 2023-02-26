e955a13d2c7b0fe51d56607cf8983d19b23a5d49
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-26 20:08:09 +0100

    Changed SEO properties

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -10,17 +10,16 @@ const SEO: Component<SEOProps> = ({ title, description, image }) => {
 	return (
 		<>
 			<meta charSet="utf-8" />
-			{/* <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<title>{title ?? 'Filip Koźlicki Blog'}</title>
 			<meta
 				name="description"
 				content={
 					description ??
-					'Blog dla początkujących programistów. Znajdziesz tu głownie posty w tematyce web developmentu - HTML, CSS, JAVASCRIPT, REACT, NEXT.JS, GATSBY'
+					'A blog for beginner programmers. You will find here mainly web development posts - HTML, CSS, JAVASCRIPT, REACT, NEXT.JS, GATSBY'
 				}
 			/>
-			<meta name="image" content="../images/preview.png" />
+			<meta name="image" content={`${process.env.GATSBY_DOMAIN}/preview.png`} />
 			<link rel="icon" href="" />
 			<meta property="og:title" content={title} />
 			<meta property="og:description" content={description} />
@@ -28,17 +27,20 @@ const SEO: Component<SEOProps> = ({ title, description, image }) => {
 			<meta property="og:type" content="website" />
 			<meta property="og:image" content="../images/preview.png" />
 			<meta property="og:site_name" content="Filip Koźlicki Blog" />
-			<meta property="og:locale" content="pl_PL" />
+			<meta property="og:locale" content="en_US" />
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:title" content={title ?? 'Filip Koźlicki Blog'} />
 			<meta
 				name="twitter:description"
 				content={
 					description ??
-					'Blog dla początkujących programistów. Znajdziesz tu głownie posty w tematyce web developmentu - HTML, CSS, JAVASCRIPT, REACT, NEXT.JS, GATSBY'
+					'A blog for beginner programmers. You will find here mainly web development posts - HTML, CSS, JAVASCRIPT, REACT, NEXT.JS, GATSBY'
 				}
 			/>
-			<meta name="twitter:image" content="../images/preview.png" />
+			<meta
+				name="twitter:image"
+				content={`${process.env.GATSBY_DOMAIN}/preview.png`}
+			/>
 			<meta name="twitter:site" content="@fkozlicki" />
 			<meta name="twitter:creator" content="@fkozlicki" />
 		</>
