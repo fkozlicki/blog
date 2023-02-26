eed523ae1a147858b7d6094c370c9e15a06472e9
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-26 15:32:04 +0100

    Fixed wave background colors

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,13 @@ import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
 import { useThemeContext } from '../../context/ThemeContext';
 
 const About = () => {
-	const [{ theme }] = useThemeContext();
+	const [
+		{
+			theme: {
+				colors: { backgroundPrimary },
+			},
+		},
+	] = useThemeContext();
 
 	return (
 		<S.AboutSection>
@@ -42,12 +48,15 @@ const About = () => {
 					</a>
 				</S.AboutSocials>
 			</S.AboutContentWrapper>
-			<StaticImage
-				src="../../images/wave.svg"
-				alt=""
-				className="about-wave"
-				layout="fullWidth"
-			/>
+			<S.WaveWrapper>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+					<path
+						fill={backgroundPrimary}
+						fillOpacity="1"
+						d="M0,192L48,208C96,224,192,256,288,266.7C384,277,480,267,576,250.7C672,235,768,213,864,192C960,171,1056,149,1152,165.3C1248,181,1344,235,1392,261.3L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+					></path>
+				</svg>
+			</S.WaveWrapper>
 		</S.AboutSection>
 	);
 };
