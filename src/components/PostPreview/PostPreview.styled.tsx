2e6729dda06b50356668253753a408dc9e27f08d
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-26 15:34:33 +0100

    Removed boilerplate code

--- src/components/PostPreview/PostPreview.styled.tsx
+++ src/components/PostPreview/PostPreview.styled.tsx
@@ -17,34 +17,6 @@ export const PostPreview = styled.article`
 	}
 `;
 
-export const PostPreviewBadges = styled.div`
-	display: flex;
-	gap: 5px;
-	margin-bottom: 15px;
-`;
-
-export const PostPreviewBadge = styled.span`
-	padding: 2px 6px;
-	font-size: 13px;
-	color: ${({ theme: { colors } }) => colors.fontPrimary};
-	border: 1px solid gray;
-	border-radius: 9999px;
-	background: linear-gradient(
-			${({ theme: { colors } }) => colors.hoverPrimary} 0 0
-		)
-		var(--p, 0) / var(--p, 0) no-repeat;
-	transition: 0.3s, background-position 0s;
-
-	&:not(:first-child) {
-		margin-left: 8px;
-	}
-
-	&:hover {
-		--p: 100%;
-		color: ${({ theme: { colors } }) => colors.fontSecondary};
-	}
-`;
-
 export const PostPreviewExcerpt = styled.p`
 	font-size: 18px;
 	line-height: 1.3;
